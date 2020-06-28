1a338ccaff18cfae2d08a08226a8f6df56d9a392
Himanshu Parchand <himan2104@gmail.com>
2020-06-29 01:04:59 +0530

    +comments in for easy understanding

--- 2DRayTracing/Defs.hpp
+++ 2DRayTracing/Defs.hpp
@@ -11,4 +11,4 @@ const unsigned int frameRateCap = 90; //frame rate limiter
 const float intensity = 200; //intensity/density of rays. suggested values(200, 10000)
 const sf::Color rayColor(255,0,255); //color of ray
 const sf::Color wallColor(0,255,0); // color of walls
-const unsigned int randomWallCount = 7; // number of randomly generated walls
+const unsigned int randomWallCount = 7; // number of randomly generated walls 
